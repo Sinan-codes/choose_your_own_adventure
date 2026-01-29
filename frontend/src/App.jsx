57ec63c01bcd0bc1fa96322ac88bb788beb99a0f
Mohamed Kassim Sinan <mohamed.kassim.sinan@gmail.com>
2026-01-29 08:41:50 +0400

    frontend build

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import StoryLoader from './components/StoryLoader'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <Router>
+      <div className='app-container'>
+        <header>
+          <h1>Interactive Story Generator</h1>
+        </header>
+        <main>
+          <Routes>
+            <Route path={"/story/:id"} element={<StoryLoader />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  )
+}
+
+export default App
